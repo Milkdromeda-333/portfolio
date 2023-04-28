ec8d7cd5f24ffd5bdc3836be6232f3fa009a18e1
Anjanique <anjaniquemackey@gmail.com>
2023-04-28 14:29:12 -0700

    update: hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,13 +19,13 @@ function Hero() {
                 <p> I'm a
                     <span className="hero__special-words"> MERN Stack Web Developer</span>, 
                     <span className="hero__bolded-upon-hover"> book-worm</span>,
-                    <span className="hero__bolded-upon-hover"> space-geek</span>,
+                    <span className="hero__bolded-upon-hover"> space-geek</span>, and 
                     <span className="hero__bolded-upon-hover"> junk-food connoisseur</span>.
-                    I got into this field because 
-                    <span className="hero__bolded-upon-hover"> I love a good challenge </span>
-                    and
-                    <span className="hero__bolded-upon-hover"> I enjoy solving problems </span>
-                    , but I soon discovered that it's also a really creative outlet for me. My ultimate goal is to use my skills to make a positive impact on the world and create software that people find useful and enjoyable. I'm always curious and eager to learn more, and I'm excited to use my skills to create awesome stuff.
+                    I initially fell in love with coding because
+                    <span className="hero__bolded-upon-hover"> I love a good challenge</span>
+                    , but I soon discovered that it's also a creative outlet for me. My ultimate goal is to use my skills to make a positive impact on the world and create software that people find useful and enjoyable. I'm always
+                    <span className="hero__bolded-upon-hover"> curious and eager to learn more</span>
+                    , and I'm excited to use my skills to create awesome stuff.
                 </p>
 
                 <a href="https://drive.google.com/file/d/1HaJuwxlPx4OjBeITC5tJ5XZj2uwTlMt7/view?usp=share_link" target="_blank" className="hero__button">
